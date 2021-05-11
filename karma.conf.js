f9899e6f5b56e54edc7d8437c73800a100b71cf1
Pradeep Singh <pradeep.dahima@gmail.com>
2021-05-11 12:22:09 +0530

    update chrome setting

--- karma.conf.js
+++ karma.conf.js
@@ -8,25 +8,17 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-sonarqube-unit-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    sonarQubeUnitReporter: {
-      sonarQubeVersion: 'LATEST',
-      outputFile: 'reports/ut_report.xml',
-      overrideTestDescription: true,
-      testPaths: ['./src'],
-      testFilePattern: '.spec.ts',
-      useBrowserName: false
-    },
-    reporters: ['sonarqubeUnit'],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: [ 'html', 'lcovonly', 'text-summary' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -38,6 +30,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: true
   });
 };
